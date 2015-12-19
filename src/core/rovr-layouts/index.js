0cbd5ce55a9637353127b0bb2d6c682e658838ba
Jake Deichert <jakedeichert@users.noreply.github.com>
2015-12-19 11:40:10 -0500

    Only load layouts that end with .html
    Realistically, you should never have a layout that isn't an html
    page...
    
    Rovr is a static site generator. Having layouts for other file types
    doesn't really align with the goals of being a site generator.

--- src/core/rovr-layouts/index.js
+++ src/core/rovr-layouts/index.js
@@ -5,7 +5,7 @@ export default function plugin() {
         pre: function(files, rovr, callback) {
             for (let f in files) {
                 // Check if it's in the layouts folder.
-                if (f.match(`^_layouts`)) {
+                if (f.match(`^_layouts`) && f.match('.html$')) {
                     rovr.layouts[f] = {
                         name: path.basename(f).replace('.html', ''),
                         fileData: files[f]
